b6a20f7a6c83479f181f09a08c37251d52e68703
venelin-p-petrov <venelin.p.petrov@gmail.com>
2013-08-26 21:42:18 +0300

    Finished homework "Pages and Navigation"

---
+++ 8. WinJS-Pages-and-Navigation/BulgariaApp/js/default.js
@@ -0,0 +1,34 @@
+﻿// For an introduction to the Navigation template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232506
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    var nav = WinJS.Navigation;
+
+    app.addEventListener("activated", function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+
+            if (app.sessionState.history) {
+                nav.history = app.sessionState.history;
+            }
+            args.setPromise(WinJS.UI.processAll().then(function () {
+                if (nav.location) {
+                    nav.history.current.initialPlaceholder = true;
+                    return nav.navigate(nav.location, nav.state);
+                } else {
+                    return nav.navigate(Application.navigator.home);
+                }
+            }));
+        }
+    });
+
+    app.oncheckpoint = function (args) {
+        app.sessionState.history = nav.history;
+    };
+
+    app.start();
+})();
---
+++ 8. WinJS-Pages-and-Navigation/BulgariaApp/pages/home/home.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            WinJS.Utilities.query("#nav-history", element).listen("click", function () {
+                WinJS.Navigation.navigate("ms-appx:///pages/history/history.html");
+            });
+
+            WinJS.Utilities.query("#nav-geography", element).listen("click", function () {
+                WinJS.Navigation.navigate("ms-appx:///pages/geography/geography.html");
+            });
+
+            WinJS.Utilities.query("#nav-politics", element).listen("click", function () {
+                WinJS.Navigation.navigate("ms-appx:///pages/politics/politics.html");
+            });
+
+            WinJS.Utilities.query("#nav-economy", element).listen("click", function () {
+                WinJS.Navigation.navigate("ms-appx:///pages/economy/economy.html");
+            });
+
+            WinJS.Utilities.query("#nav-demographics", element).listen("click", function () {
+                WinJS.Navigation.navigate("ms-appx:///pages/demographics/demographics.html");
+            });
+
+            WinJS.Utilities.query("#nav-culture", element).listen("click", function () {
+                WinJS.Navigation.navigate("ms-appx:///pages/culture/culture.html");
+            });
+        }
+    });
+})();
