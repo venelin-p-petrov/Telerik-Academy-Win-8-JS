fcc9fcd4f8c8b263e01a28f93cdb58cc3bb6cfcc
venelin-p-petrov <venelin.p.petrov@gmail.com>
2013-08-27 21:12:22 +0300

    Finished homework "App Storage"

---
+++ 10. App-Storage/OptimizedPrimeCalculator/js/default.js
@@ -0,0 +1,78 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        var localSettings = Windows.Storage.ApplicationData.current.localSettings;
+
+        if (!localSettings.values['workerNumber']) {
+            localSettings.values['workerNumber'] = 3;
+        }
+
+        var calculator = new Calculator.PrimeNumbersCalculator(localSettings.values['workerNumber']);
+
+        var calculatePrimesUpToButton = document.getElementById("primes-upto-btn");
+        var primesUpTo = document.getElementById("primes-upto");
+
+        var calculatePrimesFirstButton = document.getElementById("primes-first-btn");
+        var primesFirst = document.getElementById("primes-first");
+
+        var calculatePrimesBetweenButton = document.getElementById("primes-between-btn");
+        var primesLower = document.getElementById("primes-lower");
+        var primesUpper = document.getElementById("primes-upper");
+
+        var workerNumber = document.getElementById("worker-number");
+        var workerNumberButton = document.getElementById("set-worker-number");
+
+        var clearOutputButton = document.getElementById("clear-output");
+
+        workerNumber.value = localSettings.values['workerNumber'];
+
+        calculatePrimesUpToButton.addEventListener("click", function () {
+            calculator.calculateUpToAsync(primesUpTo.value).then(function (data) {
+                printResult(data.method + data.primes.join(", "));
+            }, function (error) {
+                printResult(error);
+            });
+        });
+
+        calculatePrimesFirstButton.addEventListener("click", function () {
+            calculator.calculateFirstNAsync(primesFirst.value).then(function (data) {
+                printResult(data.method + data.primes.join(", "));
+            }, function (error) {
+                printResult(error);
+            });
+        });
+
+        calculatePrimesBetweenButton.addEventListener("click", function () {
+            calculator.calculateBetweenAsync(primesLower.value, primesUpper.value).then(function (data) {
+                printResult(data.method + data.primes.join(", "));
+            }, function (error) {
+                printResult(error);
+            });
+        });
+
+        workerNumberButton.addEventListener("click", function () {
+            localSettings.values['workerNumber'] = workerNumber.value;
+            calculator = new Calculator.PrimeNumbersCalculator(localSettings.values['workerNumber']);
+        });
+
+        clearOutputButton.addEventListener("click", function () {
+            document.getElementById("output").innerHTML = "";
+        });
+
+        var printResult = function (text) {
+            var resultParagraph = document.createElement("p");
+            resultParagraph.innerText = text;
+            document.getElementById("output").appendChild(resultParagraph);
+        }
+    };
+
+    app.start();
+})();
---
+++ 10. App-Storage/OptimizedPrimeCalculator/js/primeNumbersCalculator.js
@@ -0,0 +1,147 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+
+WinJS.Namespace.define("Calculator", {
+    PrimeNumbersCalculator: WinJS.Class.define(
+        function (workersNumber) {
+            this.workersNumber = workersNumber;
+            this.primesWorkers = new Array(this.workersNumber);
+            this.takenWorkers = new Array(this.workersNumber);
+
+            for (var i = 0; i < workersNumber; i++) {
+                this.primesWorkers[i] = new Worker("/js/primesWorker.js");
+                this.takenWorkers[i] = false;
+            }
+        }, {
+            getWorkerNumber: function () {
+                for (var i = 0; i < this.workersNumber; i++) {
+                    if (!this.takenWorkers[i]) {
+                        return i;
+                    }
+                }
+                return null;
+            },
+            saveResultAsync: function (meta, data) {
+                var localFolder = Windows.Storage.ApplicationData.current.localFolder;
+                var filename;
+
+                switch (meta.method) {
+                    case "upto":
+                        filename = "upto-" + meta.upto + ".txt";
+                        break;
+                    case "firstN":
+                        filename = "firstN-" + meta.firstN + ".txt";
+                        break;
+                    case "between":
+                        filename = "between-" + meta.first + "-" + meta.last + ".txt";
+                        break;
+                }
+                localFolder.createFileAsync(filename, Windows.Storage.CreationCollisionOption.replaceExisting).done(function (file) {
+                    Windows.Storage.FileIO.writeTextAsync(file, JSON.stringify(data));
+                });
+            },
+            loadResultAsync: function (meta) {
+                var localFolder = Windows.Storage.ApplicationData.current.localFolder;
+                var filename;
+
+                switch (meta.method) {
+                    case "upto":
+                        filename = "upto-" + meta.upto + ".txt";
+                        break;
+                    case "firstN":
+                        filename = "firstN-" + meta.firstN + ".txt";
+                        break;
+                    case "between":
+                        filename = "between-" + meta.first + "-" + meta.last + ".txt";
+                        break;
+                }
+
+                return new WinJS.Promise(function (complete, error) {
+                    localFolder.getFileAsync(filename).then(function (file) {
+                        Windows.Storage.FileIO.readTextAsync(file).done(function (text) {
+                            complete(JSON.parse(text));
+                        });
+                    }, function () {
+                        complete(null);
+                    });
+                });
+            },
+            calculateUpToAsync: function (upto) {
+                var self = this;
+                return new WinJS.Promise(function (complete, error) {
+                    self.loadResultAsync({ method: "upto", upto: upto }).then(function (saved) {
+                        if (saved) {
+                            complete(saved);
+                        }
+                        else {
+                            var workerNumber = self.getWorkerNumber();
+                            if (workerNumber !== null) {
+                                self.primesWorkers[workerNumber].onmessage = function (event) {
+                                    self.takenWorkers[event.data.number] = false;
+
+                                    self.saveResultAsync({ method: "upto", upto: upto }, event.data);
+
+                                    complete(event.data);
+                                };
+                                self.takenWorkers[workerNumber] = true;
+                                self.primesWorkers[workerNumber].postMessage({ method: "upto", upto: upto, number: workerNumber });
+                            } else {
+                                error("Only " + self.workersNumber + " requests are allowed at one time.")
+                            }
+                        }
+                    });
+                });
+            },
+            calculateFirstNAsync: function (n) {
+                var self = this;
+                return new WinJS.Promise(function (complete, error) {
+                    self.loadResultAsync({ method: "firstN", firstN: n }).then(function (saved) {
+                        if (saved) {
+                            complete(saved);
+                        }
+                        else {
+                            var workerNumber = self.getWorkerNumber();
+                            if (workerNumber !== null) {
+                                self.primesWorkers[workerNumber].onmessage = function (event) {
+                                    self.takenWorkers[event.data.number] = false;
+
+                                    self.saveResultAsync({ method: "firstN", firstN: n }, event.data);
+
+                                    complete(event.data);
+                                };
+                                self.takenWorkers[workerNumber] = true;
+                                self.primesWorkers[workerNumber].postMessage({ method: "firstN", n: n, number: workerNumber });
+                            } else {
+                                error("Only " + self.workersNumber + " requests are allowed at one time.")
+                            }
+                        }
+                    });
+                });
+            },
+            calculateBetweenAsync: function (first, last) {
+                var self = this;
+                return new WinJS.Promise(function (complete, error) {
+                    self.loadResult({ method: "between", first: first, last: last }).then(function (saved) {
+                        if (saved) {
+                            complete(saved);
+                        }
+                        else {
+                            var workerNumber = self.getWorkerNumber();
+                            if (workerNumber !== null) {
+                                self.primesWorkers[workerNumber].onmessage = function (event) {
+                                    self.takenWorkers[event.data.number] = false;
+
+                                    self.saveResultAsync({ method: "between", first: first, last: last }, event.data);
+
+                                    complete(event.data);
+                                };
+                                self.takenWorkers[workerNumber] = true;
+                                self.primesWorkers[workerNumber].postMessage({ method: "between", first: first, last: last, number: workerNumber });
+                            } else {
+                                error("Only " + self.workersNumber + " requests are allowed at one time.")
+                            }
+                        }
+                    });
+                });
+            }
+        })
+});
